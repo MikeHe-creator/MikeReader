0c52a6a777aee17d54486238b76cd85b3b5a77c4
MikeHe-creator <123228278+MikeHe-creator@users.noreply.github.com>
2024-01-16 07:27:06 +0800

    注册之密码2

--- MikeReader/components/register_step1.vue
+++ MikeReader/components/register_step1.vue
@@ -1,33 +1,33 @@
 <template>
     <form class="flex flex-col  lg:grid lg:grid-cols-2 lg:gap-2 lg:items-start text-center p-8 " name="loginlist">
         <!--user name-->
-        <p class=" flex justify-start text-white">User Name:</p>
+        <label class=" flex justify-start text-white">User Name:</label>
         <input type="text" placeholder="User Name" class="ml-[0.1em]" v-model="username">
 
         <!--email-->
-        <p class=" flex justify-start text-white">Email:</p>
+        <label class=" flex justify-start text-white">Email:</label>
         <input type="email" placeholder="Email" class="ml-[0.1em]" v-model="email">
 
         <!--phone number-->
-        <p class=" flex justify-start text-white">Phone Number:</p>
+        <label class=" flex justify-start text-white">Phone Number:</label>
         <input type="tel" ref="phoneInput" placeholder="Phone Number" class="ml-[0.1em]" v-model="phonenum" maxlength="11">
 
         <!--password-->
-        <p class=" flex justify-start text-white">Password:</p>
+        <label class=" flex justify-start text-white ">Password:</label>
         <div class=" relative flex justify-end ">
-            <input :type="showPassword ? 'text' : 'password'" placeholder="Password" class="ml-[0.1em] w-full" v-model="password" ref="show1">
-            <button class=" bg-white relative" @click="displayeye"> 
-                <img src="./svg/svgviewer-output.svg" class=" block" ref="openeye"> 
-                <img src="./svg/svgviewer-output (1).svg" class=" hidden" ref="closedeye">
+            <input :type="showPassword ? 'text' : 'password'" placeholder="Password" class="ml-[0.1em] w-full" v-model="password">
+            <button type="button" class=" bg-white relative" @click="displayeye"> 
+                <img v-if="showPassword" src="./svg/svgviewer-output (1).svg" alt="Password Hidden">
+                <img v-else src="./svg/svgviewer-output.svg" alt="Password Visible">
             </button>
         </div>
-        <p class=" flex justify-start text-white">Rewrite Password:</p>
+        <label class=" flex justify-start text-white ">Rewrite Password:</label>
         <div class=" relative flex justify-end ">
             <input :type="showPassword2 ? 'text' : 'password'" placeholder="Rewrite Password" class="ml-[0.1em] w-full" v-model="repassword">
-            <button class=" bg-white relative" @click="displayrepas">
-                <img src="./svg/svgviewer-output.svg" class=" block" ref="repasseye"> 
-                <img src="./svg/svgviewer-output (1).svg" class=" hidden" ref="reclosed">
-            </button>   
+            <button type="button" class=" bg-white relative" @click="displayrepas"> 
+                <img v-if="showPassword2" src="./svg/svgviewer-output (1).svg" alt="Password Hidden">
+                <img v-else src="./svg/svgviewer-output.svg" alt="Password Visible">
+            </button>
         </div>
     </form>
     <div>
@@ -74,13 +74,9 @@ const email=ref("");
 const phonenum=ref("");
 const password=ref("");
 const showPassword = ref(false);
-const openeye=ref(null);
-const closedeye=ref(null);
 const repassword=ref("");
-const show1=ref();
 const showPassword2=ref(false);
-const repasseye=ref(null);
-const reclosed=ref(null);
+
 
 onMounted(() => {
    window.intlTelInput(phoneInput.value, {
@@ -101,13 +97,9 @@ function validateForm(){
     };
     var phonenumval=phonenum.value
     if (!validatePhone(phonenumval)||phonenumval.length<4 || phonenumval.length>11 ) {
-        alert('请输入有效的电话号码');
+        alert('Please enter correct phone number!');
         return false;
     }
-    if (password !== confirmPassword) {
-        alert('The two input secrets do not agree!');
-        return false;
-    };
 
     function validateEmail(email) {
         var emailRegex = /^([a-zA-Z0-9_-])+@([a-zA-Z0-9_-])+(\.[a-zA-Z0-9_-])+/;
@@ -120,24 +112,10 @@ function validateForm(){
 
 };
 
-function displayeye(){
-    showPassword.value = !showPassword.value;
-    if (showPassword.value) {
-        openeye.value.classList.remove('hidden');
-        closedeye.value.classList.add('hidden');
-    } else {
-        openeye.value.classList.add('hidden');
-        closedeye.value.classList.remove('hidden');
-    }
+function displayeye() {
+  showPassword.value = !showPassword.value;
 }
 function displayrepas(){
-    showPassword2.value = !showPassword2.value;
-    if (showPassword2.value) {
-        repasseye.value.classList.remove('hidden');
-        reclosed.value.classList.add('hidden');
-    } else {
-        repasseye.value.classList.add('hidden');
-        reclosed.value.classList.remove('hidden');
-    }
+  showPassword2.value = !showPassword2.value;
 }
 </script>
--- MikeReader/pages/videoplayer.vue
+++ MikeReader/pages/videoplayer.vue
@@ -1,52 +1,23 @@
 <template>
-    <!--password-->
-    <p class=" flex justify-start ">Password:</p>
-        <div >
-            <input :type="showPassword ? 'text' : 'password'" placeholder="Password" class="ml-[0.1em] " v-model="password" ref="show1">
-            <button type="button" class="  relative" @click="displayeye"> 
-                <img src="D:\document file\JavaScript\JavaScrpit\vuep01\MikeReader\components\svg\svgviewer-output.svg" class=" block" ref="openeye"> 
-                <img src="D:\document file\JavaScript\JavaScrpit\vuep01\MikeReader\components\svg\svgviewer-output (1).svg" class=" hidden" ref="closedeye">
-            </button>
-        </div>
-        <p class=" flex justify-start ">Rewrite Password:</p>
-        <div >
-            <input :type="showPassword2 ? 'text' : 'password'" placeholder="Rewrite Password" class="ml-[0.1em]" v-model="repassword">
-            <button type="button" class="  relative" @click="displayrepas">
-                <img src="D:\document file\JavaScript\JavaScrpit\vuep01\MikeReader\components\svg\svgviewer-output.svg" class=" block" ref="repasseye"> 
-                <img src="D:\document file\JavaScript\JavaScrpit\vuep01\MikeReader\components\svg\svgviewer-output (1).svg" class=" hidden" ref="reclosed">
-            </button>   
-        </div>
-</template>
-<script setup>
-    import { ref } from 'vue';
-    const password=ref("");
-    const showPassword = ref(false);
-    const openeye=ref(null);
-    const closedeye=ref(null);
-    const repassword=ref("");
-    const show1=ref();
-    const showPassword2=ref(false);
-    const repasseye=ref(null);
-    const reclosed=ref(null);
-
-    function displayeye(){
-        showPassword.value = !showPassword.value;
-        if (showPassword.value) {
-            openeye.value.classList.remove('hidden');
-            closedeye.value.classList.add('hidden');
-        } else {
-            openeye.value.classList.add('hidden');
-            closedeye.value.classList.remove('hidden');
-        }
-    }
-    function displayrepas(){
-        showPassword2.value = !showPassword2.value;
-        if (showPassword2.value) {
-            repasseye.value.classList.remove('hidden');
-            reclosed.value.classList.add('hidden');
-        } else {
-            repasseye.value.classList.add('hidden');
-            reclosed.value.classList.remove('hidden');
-        }
-    }
-</script>
+    <p>jsdgf:</p>
+    <button type="button" @click="displayeye">
+      <img :src="eyesvg"> asdfhhjgjdgsf
+    </button>
+  </template>
+  
+  <script setup>
+  import { ref } from 'vue';
+  
+  const images = {
+    normal: '@/components/svg/svgviewer-output.svg',
+    toggled: '@/components/svg/svgviewer-output (1).svg'
+  };
+  
+  const eyesvg = ref(images.normal);
+  
+  function displayeye() {
+    // 切换图像
+    eyesvg.value = eyesvg.value === images.normal ? images.toggled : images.normal;
+  }
+  </script>
+  

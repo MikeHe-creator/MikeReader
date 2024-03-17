c39ee7808d32d4e474be6454a6c3f7fbf4d06ede
MikeHe-creator <123228278+MikeHe-creator@users.noreply.github.com>
2024-03-17 14:29:18 +0800

    pdf缩略图目录完成

--- MikeReader/Backendin/pdfreader.py
+++ MikeReader/Backendin/pdfreader.py
@@ -1,17 +1,18 @@
-import fitz,os,tempfile,base64
+import fitz, os, tempfile, base64
 from flask_cors import CORS
-from flask import Flask,jsonify,request
+from flask import Flask, jsonify, request
 from PIL import Image
 
 app = Flask(__name__)
 CORS(app)
 
-@app.route('/sendpdfs',methods=['POST'])
+
+@app.route('/sendpdfs', methods=['POST'])
 def get_PDF():
     if 'book' not in request.files:
         return jsonify({'error': 'No file part in the request'}), 400
     pdffile = request.files['book']
-    print("the result of pdffile:",pdffile)
+    print("the result of pdffile:", pdffile)
     if pdffile.filename is None:
         return jsonify({'error': 'No selected file'}), 400
 
@@ -20,12 +21,12 @@ def get_PDF():
     filepath = os.path.join(upload_folder, pdffile.filename)
     pdffile.save(filepath)
 
-
-    outline,images = get_PDFoutline(filepath)
+    outline, images = get_PDFoutline(filepath)
     os.remove(filepath)
     os.removedirs("temp")
-    print("img张数：",len(images))
-    return jsonify({'outline': outline,'viewpdf':images}), 200
+    print("img张数：", len(images))
+    return jsonify({'outline': outline, 'viewpdf': images}), 200
+
 
 def get_PDFoutline(filepath):
     doc = fitz.open(filepath)
@@ -45,7 +46,7 @@ def get_PDFoutline(filepath):
                 images.append(image_base64)
     doc.close()
     print("the result of outline", outline)
-    return outline,images
+    return outline, images
 
 
 if __name__ == '__main__':
--- MikeReader/pages/bookreader.vue
+++ MikeReader/pages/bookreader.vue
@@ -16,21 +16,22 @@
         <input type="file" ref="inputbook" accept=".txt, .epub, .mobi, .pdf" class="hidden"><div class=" inline-block ml-[1em]">{{ bookname }}</div>
       </div>
       <div>
-        <div class=" bg-orange-100 lg:max-w-[85em] h-[3em] ml-[6em] mt-[1em] flex-col flex justify-center">
+        <div class=" bg-orange-100 lg:w-[85em] h-[3em] lg:ml-[6em] mt-[1em] flex-col flex justify-center sm:wl-[4em] sm:w-[10em]">
           <button @click="showcontents"><img src="./icons/svg/menu-svgrepo-com.svg" class=" w-[25px] h-[25px] ml-[0.5em]"></button>
         </div>
-        <div class="bg-black lg:max-w-[85em] h-[41em] ml-[6em] flex justify-center  overflow-auto" ref="display">
-            <div class="flex justify-center items-start ">
-              <div ref="pdfCanvas" class=""></div>
-            </div>
+        <div class="bg-black lg:w-[85em] h-[41em] lg:ml-[6em] flex justify-center overflow-auto sm:wl-[1em] sm:w-[10em]" ref="display">
+          <div class="flex justify-center items-start ">
+            <div ref="pdfCanvas" class=""></div>
+          </div>
         </div>
-        <div ref="contents" class="bg-gray-800 lg:w-[20em] h-[41em] z-10 absolute mt-[-41em] ml-[6em]">
+        <div ref="contents" class="bg-gray-800 lg:w-[20em] h-[41em] z-10 absolute mt-[-41em] lg:ml-[6em] lg:block sm:hidden sm:w-[2em] sm:wl-[1.5em]">
           <p class="text-white text-[1.5em] ml-[0.5em] font-bold inline-block" ref="menucon">Content List</p>
           <p class="hidden text-white text-[1.5em] ml-[0.5em] font-bold" ref="pages">Content View</p>
           <button ref="tridown" class="ml-[2em] brightness-0 invert inline-block" @click="translist"><img src="./icons/svg/triangle-down.svg" class="w-[2em] h-[2em]"></button>
-          <div class="border border-white w-[20em]"></div>
+          <div class="border border-white lg:w-[20em] sm:w-[10em]"></div>
           <div ref="idcontents" class=" text-white h-[38.3em] overflow-y-auto"></div>
-          <div ref="idpage" class="hidden"></div>
+          <div ref="idpage" class="hidden h-[38.3em] overflow-y-auto">
+          </div>
         </div>
       </div>
     </div>
@@ -50,6 +51,7 @@ const pdfCanvas=ref();
 const idpage=ref();
 const tridown=ref();
 const display=ref();
+const pdfroom=[];
 
 function uptobook() {
   inputbook.value.addEventListener('change', handleFileChange);
@@ -77,58 +79,96 @@ async function sendbook(selectedFile,bookname) {
           const pdfimg=response.data.viewpdf;
           const outline = response.data.outline;
           const idcontents1=idcontents.value;
-          //目录
-          if (outline.length === 0){
-            idpage.value.style.display="block";
-            idcontents.value.style.display="none";
-            pages.value.style.display="inline-block";
-            menucon.value.style.display="none";
-            tridown.value.style.display="none";
-          }else{
-            idpage.value.style.display="none";
-            idcontents.value.style.display="block";
-            pages.value.style.display="none";
-            menucon.value.style.display="inline-block";
-            tridown.value.style.display="inline-block";
-            idcontents1.innerHTML = '';
-            outline.forEach(item => {
-              const title = item[1];
-              const page = item[2];
-              const directory = document.createElement('p');
-              directory.innerHTML = `<p class=" mt-[4px] hover:bg-gray-500 cursor-pointer" id="content${page}">${title}</p>`;
-              idcontents1.appendChild(directory);
-            });
+          pdfroom.length=0;
+          for(let pdf in pdfimg){
+            pdfroom.push(pdfimg[pdf])
           }
-          jumppage(idcontents1,pdfimg);
-          //阅读
-          const pdfCanvas1=pdfCanvas.value;
-          while (pdfCanvas1.firstChild) {
-            pdfCanvas1.removeChild(pdfCanvas1.firstChild);
-          }
-          pdfimg.forEach((imageData, index) => {
-            let canvas = document.createElement('canvas');
-            canvas.style.marginBottom = '5px';
-            const ctx = canvas.getContext('2d');
-            pdfCanvas1.appendChild(canvas);
-            const img = new Image();
-            img.src = 'data:image/png;base64,' + imageData;
-            img.onload = () => {
-              const imageWidth = img.width;
-              const imageHeight = img.height;
-              canvas.width = imageWidth;
-              canvas.height = imageHeight;
-              ctx.drawImage(img, 0, 0, canvas.width, canvas.height);
-              canvas.id=`canvas${index+1}`
-            }
-            if (index === pdfimg.length - 1) {
-              console.log("All PDF pages are displayed.");
-            }
-          })
+          icontents(outline,idcontents1);
+          pdfimgs(pdfroom);
         });
   } catch (error) {
     console.error(`Meet a problem to deal with the pdf ${bookname.value} contents:`, error);
   }
 }
+
+function icontents(outline,idcontents1){
+  if (outline.length === 0){
+    idpage.value.style.display="block";
+    idcontents.value.style.display="none";
+    pages.value.style.display="inline-block";
+    menucon.value.style.display="none";
+    tridown.value.style.display="none";
+  }else{
+    idpage.value.style.display="none";
+    idcontents.value.style.display="block";
+    pages.value.style.display="none";
+    menucon.value.style.display="inline-block";
+    tridown.value.style.display="inline-block";
+    idcontents1.innerHTML = '';
+    outline.forEach(item => {
+      const title = item[1];
+      const page = item[2];
+      const directory = document.createElement('p');
+      directory.innerHTML = `<p class="mt-[4px] hover:bg-gray-500 cursor-pointer" id="content${page}">${title}</p>`;
+      idcontents1.appendChild(directory);
+    });
+  }
+  jumppage(idcontents1);
+}
+
+function pdfimgs(pdfroom) {
+  const pdfCanvas1 = pdfCanvas.value;
+  const idpageval=idpage.value;
+  const isSmallScreen = window.innerWidth <= 768;
+  // 清空旧 PDF 内容
+  pdfCanvas1.innerHTML = '';
+  idpageval.innerHTML = '';
+  // 计数器用于跟踪图像加载完成的数量
+  let imagesLoaded = 0;
+  pdfroom.forEach((imageData, index) => {
+    let canvas = document.createElement('canvas');
+    let parentDiv = document.createElement('div');
+    let canvas2 = document.createElement('canvas');
+    canvas.style.marginBottom = '5px';
+    canvas2.style.marginBottom = '5px';
+    const ctx = canvas.getContext('2d');
+    const ctx2 = canvas2.getContext('2d');
+    pdfCanvas1.appendChild(canvas);
+    parentDiv.appendChild(canvas2);
+    const img = new Image();
+    img.src = 'data:image/png;base64,' + imageData;
+    img.onload = () => {
+      const imageWidth = img.width;
+      const imageHeight = img.height;
+      canvas.width = imageWidth;
+      canvas2.width = imageWidth * 0.3;
+      canvas.height = imageHeight;
+      canvas2.width = imageWidth * 0.3;
+      //if (isSmallScreen) {
+        //canvas.style.transform = 'scale(0.68)';
+      //}
+      ctx.drawImage(img, 0, 0, canvas.width, canvas.height);
+      ctx2.drawImage(img, 0, 0, canvas2.width, canvas2.height);
+      canvas.id = `canvas${index + 1}`;
+      canvas2.id = `canvas2_${index + 1}`;
+      parentDiv.style.cssText = `margin-top: 4px; cursor: pointer; display: flex; justify-content: center; align-items: center;`;
+      parentDiv.addEventListener('mouseenter', () => {
+        parentDiv.style.backgroundColor = 'rgb(107, 114, 128)';
+      });
+      parentDiv.addEventListener('mouseleave', () => {
+        parentDiv.style.backgroundColor = 'transparent';
+      });
+      imagesLoaded++;
+      idpageval.append(parentDiv)
+      // 如果所有图像都加载完成，则输出信息
+      if (imagesLoaded === pdfroom.length) {
+        canvasjump(idpageval);
+        console.log("All PDF pages are displayed.");
+      }
+    };
+  });
+}
+
 function showcontents(){
   showornot(contents);
 }
@@ -140,23 +180,21 @@ function showornot(contents){
   }
 }
 function translist(){
-   if (menucon.value.style.display==="inline-block"){
-     menucon.value.style.display='none';
-     idcontents.value.style.display="none";
-     idpage.value.style.display="block";
-     pages.value.style.display="inline-block";
-   }else{
-     menucon.value.style.display='inline-block';
-     idcontents.value.style.display="block";
-     idpage.value.style.display="none";
-     pages.value.style.display="none";
-   }
+  if (menucon.value.style.display==="inline-block"){
+    menucon.value.style.display='none';
+    idcontents.value.style.display="none";
+    idpage.value.style.display="block";
+    pages.value.style.display="inline-block";
+  }else{
+    menucon.value.style.display='inline-block';
+    idcontents.value.style.display="block";
+    idpage.value.style.display="none";
+    pages.value.style.display="none";
+  }
 }
 
-function jumppage(idcontents1,pdfimg){
-  const totalpage=pdfimg.length;
+function jumppage(idcontents1){
   const displayval=display.value
-  console.log(totalpage);
   idcontents1.addEventListener('click',function (event){
     if (event.target.tagName === 'P') {
       const pid = event.target.getAttribute('id');
@@ -170,5 +208,19 @@ function jumppage(idcontents1,pdfimg){
       }
     }
   })
+}
+function canvasjump(idpageval){
+  const displayval=display.value
+  idpageval.addEventListener('click', function(event) {
+    const canvasid = event.target.getAttribute('id');
+    const pageMatch = canvasid.match(/canvas2_(\d+)/);
+    const pageNumber = parseInt(pageMatch[1]);
+    const targetid = `canvas${pageNumber}`;
+    const targetElement = document.getElementById(targetid);
+    if (targetElement) {
+      const offsetTop = targetElement.offsetTop;
+      displayval.scrollTop = offsetTop === 0 ? 0 : offsetTop - displayval.offsetTop;
+    }
+  });
 }
 </script>
